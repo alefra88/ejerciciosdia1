0d064ba81606cac5cb5472750abe2945af6e09aa
alefra88 <efra.rlz@gmail.com>
2023-01-14 04:35:44 -0600

    terminando el ejercicio 8

--- ejercicios5-8.js
+++ ejercicios5-8.js
@@ -28,15 +28,25 @@
 // contarPalabras("asdsdsadd21edasdas 222","2");
 
 // 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
-const palindromo = (date = "") => {
-    if (!date) return console.warn("Debes ingresar texto");
-    const re = /[\W_]/g;
-    date = date.toLowerCase().replace(re,"");
-    const reverseDate = date.split("").reverse().join("")
-    console.log(reverseDate)
-    return (date===reverseDate)?
-      console.log(`la palabra ${date} es palindromo`):console.log(`La palabra ${date} no es palindromo`)
-};
+// const palindromo = (date = "") => {
+//     if (!date) return console.warn("Debes ingresar texto");
+//     const re = /[\W_]/g;
+//     date = date.toLowerCase().replace(re,"");
+//     const reverseDate = date.split("").reverse().join("")
+//     console.log(reverseDate)
+//     return (date===reverseDate)?
+//       console.log(`la palabra ${date} es palindromo`):console.log(`La palabra ${date} no es palindromo`)
+// };
 
-palindromo("anita lava la tina");
+// palindromo("anita lava la tina");
 // 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+const eliminarChar = (text = "", patron = "") =>
+    !text
+        ? console.warn("No ingresaste texto")
+        : !patron
+        ? console.warn("No ingresaste un patròn")
+        : console.info(text.replace(new RegExp(patron, "ig"), ""));
+
+
+
+        eliminarChar("asdasd","d")
