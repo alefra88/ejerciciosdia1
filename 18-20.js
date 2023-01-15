354c3175459e31b4af38d4c0b6d62e8223e44201
alefra88 <efra.rlz@gmail.com>
2023-01-15 06:27:08 -0600

    resolviendo el ejercicio 18

--- 18-20.js
+++ 18-20.js
@@ -1,3 +1,14 @@
 // 18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
+const conVocCon = (text = undefined) => {
+    if (text === undefined) return console.warn("ingresa una cadena de texto");
+    if (typeof text === "number")
+        return console.warn("el texto no puede ser numero");
+    const conVoc = text.match(/[aeiou]/gi).length;
+    const conCon = text.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
+    return console.info(
+        `el numero de vocales es: ${conVoc} el numero de consonantes es: ${conCon}`
+    );
+};
+conVocCon("ASDAS");
 // 19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
 // 20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
