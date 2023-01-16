f7ab3cdbc02357bebf99e3005cccbd3fdfa906d5
alefra88 <efra.rlz@gmail.com>
2023-01-16 01:35:48 -0600

    creando el ejercicio 20.1 fusiòn del ejercicio 19 y 20

--- ejercicios18-20.js
+++ ejercicios18-20.js
@@ -11,7 +11,8 @@ const conVocCon = (text = undefined) => {
 };
 conVocCon("ASDAS");
 // 19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
-const valNom = (nom) => {
+const valNom = (nom="") => {
+    if (!nom) return console.warn("Ingresa un nombre");
     const nomReg = /^[a-zA-Z\s']+$/;
     const isValid = nomReg.test(nom);
     return console.info(isValid);
@@ -25,3 +26,17 @@ const valEma = (email) => {
     return console.info(isvalid);
 };
 valEma("alan@performance.com");
+
+
+//20.1 FUSION del ejercicio 19 y el 20
+const valNomAndEma = (nom="",ema="") => {
+    if (!nom) return console.warn("Ingresa un nombre");
+    if (!ema) return console.warn("Ingresa un email");
+    const nomRegf = /^[a-zA-Z\s']+$/;
+    const emaRegf = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValidNom = nomRegf.test(nom);
+    const isValidEma = emaRegf.test(ema)
+    return console.info(`El nombre ${nom} es ${isValidNom} \nEl email ${ema} es ${isValidEma}`);
+};
+
+valNomAndEma("chucho Aldama","cerdo@cerdito.com")
